dae58a224b4b885845278cefbd1d25989da65b2a
Eduard0177 <131684798+Eduard0177@users.noreply.github.com>
2024-08-23 10:21:28 -0300

    Update LivroController.java

--- 0908-POO-Class-Autor-main/demo/src/main/java/com/aos/aula02/demo/controller/LivroController.java
+++ 0908-POO-Class-Autor-main/demo/src/main/java/com/aos/aula02/demo/controller/LivroController.java
@@ -1,5 +1,18 @@
 package com.aos.aula02.demo.controller;
 
+@RequestController
+@RequestMapping("api/if/livro/")
 public class LivroController {
+    @Autowired
+    public LivroService livroService;
+
+    @PostMapping("save/")
+    public Livro inserirLivro(@RequestBody Livro livro) {
+        return livroService.inserir(livro);
+    }
+    @GetMapping
+    public List<Livro> buscarTodosLivros() {
+        return livroService.buscarTodos();
+    }
 
 }
